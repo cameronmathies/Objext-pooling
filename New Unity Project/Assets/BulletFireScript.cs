b86d27384ce3ae2cfd9291bfe587add3f5b63a5a
cameronmathies <cmathies@IT202.mr.preble.com>
2015-09-17 12:47:50 -0400

    Base

---
+++ New Unity Project/Assets/BulletFireScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BulletFireScript : MonoBehaviour {
+
+	public float fireTime = .05f;
+	public GameObject bullet;
+
+	public int pooledAmount = 20;
+	List<GameObject> bullets;
+
+	// Use this for initialization
+	void Start () {
+		bullets = new List<GameObject> ();
+		for(int i = 0; i < pooledAmount; i++) {
+			GameObject obj = (GameObject)Instantiate(bullet);
+			obj.SetActive(false);
+			bullets.Add(obj);
+		}
+		if (Input.GetKey (KeyCode.Space)) {
+			InvokeRepeating ("Fire", fireTime, fireTime);
+		}
+	}
+	
+	// Update is called once per frame
+	void Fire () {
+		for(int i = 0; i < bullets.Count; i++)
+		{
+			if(!bullets[i].activeInHierarchy)
+			{
+				Input.GetKey(KeyCode.Space);
+				bullets[i].transform.position = transform.position;
+				bullets[i].transform.rotation = transform.rotation;
+				bullets[i].SetActive(true);
+				break;
+			}
+		}
+
+
+	}
+}
